817fe1534885e548225e026620c510801bde80cd
Leonel Aguirre <leonel.aguirre77@gmail.com>
2019-03-26 17:56:42 -0600

    Functions added to dimension inputs container and started to work with the inputs of the matrix

--- public/js/master.js
+++ public/js/master.js
@@ -1,13 +1,39 @@
 $('.dimension-input-container').on('keypress', '.input',(e) => {
-  numberFilter(e, 30);
+  numberFilter(e, 10);
 });
 
 $('.dimension-input-container').on('input', '.input',() => {
   checkGenButton();
 });
 
+$('.dimension-input-container').on('input', '#matAn, #matBm',(e) => {
+  completeANBM(e);
+});
+
 $('.btn-matGen').on('click', () => {
-  console.log("pressed!");
+  console.log("GENERATE MATRIX!");
+  var Am = $('#matAm').val();
+  var AmBn = $('#matAn').val();
+  var Bn = $('#matBn').val();
+
+  console.log(Am, AmBn, Bn);
+
+  var matAHtml;
+  var matBHtml;
+
+
+});
+
+$('.btn-reset').on('click', () => {
+  console.log("RESET!");
+  $('#matAm').val('');
+  $('#matAn').val('');
+  $('#matBm').val('');
+  $('#matBn').val('');
+
+  $('.btn-matGen').attr('disabled', 'disabled');
+  $('.btn-result').attr('disabled', 'disabled');
+  $('.btn-reset').attr('disabled', 'disabled');
 });
 
 //Prevent inputs of getting non-numerical values and limits it to an specific value.
@@ -27,7 +53,7 @@ function numberFilter(e, limit){
     }
 }
 
-//Checks if text inputs are all filled and enables the gen matrix button.
+//Checks if text inputs are all filled and enables buttons.
 function checkGenButton(){
   var AmValueLenght = $('#matAm').val().length;
   var AnValueLenght = $('#matAn').val().length;
@@ -40,4 +66,22 @@ function checkGenButton(){
     $('.btn-matGen').attr('disabled', 'disabled');
   }
 
+  if(AmValueLenght || AnValueLenght || BmValueLenght || BnValueLenght){
+    $('.btn-reset').removeAttr('disabled');
+  } else {
+    $('.btn-reset').attr('disabled', 'disabled');
+  }
+
+}
+
+//Knowing that n of matrix A and m of matrix B should be equal to succesfully
+//multiply them, this function watch both inputs and makes its values the same.
+function completeANBM(e){
+  var id = e.target.id;
+
+  if(id == 'matAn'){
+    $('#matBm').val(e.target.value);
+  } else {
+    $('#matAn').val(e.target.value);
+  }
 }
